726d890d7bab7e2008f96799b2e2a47436340fab
Luis Machado Reis <luis.reis@trustly.com>
2023-10-26 19:00:01 -0300

    first commit

---
+++ src/test/java/com/example/prometheus/cluster/demo/PrometheusClusterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prometheus.cluster.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusClusterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
